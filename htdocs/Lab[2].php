23939396f75105cf8360da3fa023e42739472f85
MahimSarkar420442 <mahimsarkar42042@gmail.com>
2022-06-07 16:49:39 +0600

    Lab2 [php]

---
+++ htdocs/Lab[2].php
@@ -0,0 +1,23 @@
+
+    Answer = 01
+    ===========
+
+<?php 
+
+	$length=7;
+    $width=8;
+
+    $rectangle = ($length*$width);
+    $perimeter = 2 * ($length + $width);
+
+    echo " Area of a Rectangle =".$rectangle."<br><br>";
+    echo " Perimeter of a Rectangle =".$perimeter;
+
+?>
+
+    Answer = 02
+    ===========
+
+<?php
+
+?>
